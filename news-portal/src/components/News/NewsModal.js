8c2eafd249201ca2a0c4ffe8edc741ee919724c6
Dima Kolyas <dimakol@gmail.com>
2020-10-03 02:17:45 +0300

    fix(NewsModal): add rel=noopener noreferrer to href target=_blank

--- news-portal/src/components/News/NewsModal.js
+++ news-portal/src/components/News/NewsModal.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
 
 /**
  * Modal window that shows the full article of the latest news.
- * @param {*} props 
+ * @param {*} props
  */
-const NewsModal = props => {
-    return (
-        <Modal 
-            size="lg"
-            scrollable
-            show={props.show} 
-            onHide={props.handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>{props.title}</Modal.Title>
-            </Modal.Header>
-            {props.description || props.url || props.urlToImage ?
-                <Modal.Body>
-                    <p>{props.description}</p>
-                    <p className="center-text"><a href={props.url} target="_blank">The direct URL to the article.</a></p>
-                    {props.urlToImage ?
-                        <img src={props.urlToImage} 
-                            alt='Article Img'
-                            className="medium center-image">
-                        </img> : null
-                    }
-                </Modal.Body> : null 
-            }
-            <Modal.Footer>
-                <Button variant="primary" onClick={props.handleClose}>
-                    Close
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
-}
+const NewsModal = (props) => {
+  return (
+    <Modal size="lg" scrollable show={props.show} onHide={props.handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>{props.title}</Modal.Title>
+      </Modal.Header>
+      {props.description || props.url || props.urlToImage ? (
+        <Modal.Body>
+          <p>{props.description}</p>
+          <p className="center-text">
+            <a href={props.url} target="_blank" rel="noopener noreferrer">
+              The direct URL to the article.
+            </a>
+          </p>
+          {props.urlToImage ? (
+            <img
+              src={props.urlToImage}
+              alt="Article Img"
+              className="medium center-image"
+            ></img>
+          ) : null}
+        </Modal.Body>
+      ) : null}
+      <Modal.Footer>
+        <Button variant="primary" onClick={props.handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
 export default NewsModal;
