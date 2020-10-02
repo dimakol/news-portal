b798943a8404139eda7511a9031f8e883a682d4c
Dima Kolyas <dimakol@gmail.com>
2020-10-03 02:10:54 +0300

    feat(finance): change the stock to Tesla

--- core/finance.js
+++ core/finance.js
@@ -1,42 +1,41 @@
 //finance.js
 
 const axios = require("axios");
-const { financeKey } = require('../config');
+const { financeKey } = require("../config");
 
 var interval;
 // update per 5 minutes
 const updateTime = 5 * 60 * 1000;
 
 // function that gets the JSON result for intraday time series of the equity specified from Alpha Vantage API and emit it to client
-const getFinanceApiAndEmit = async socket => {
-    try {
-        // getting the data from Alpha Vantage API
-        const response = await axios.get("https://www.alphavantage.co/query", {
-            params: {
-                function: 'TIME_SERIES_INTRADAY',
-                symbol: 'NDAQ',
-                interval: '5min',
-                apikey: financeKey
-            }
-        });
-        // API call frequency is 5 calls per minute and 500 calls per day.
-        if (!response.data.hasOwnProperty('Note'))
-            // emitting a new message. It will be consumed by the client
-            socket.emit("Finance", response.data);
-    } 
-    catch (error) {
-        console.error(`Finance Error: ${error.code}`);
-    }
+const getFinanceApiAndEmit = async (socket) => {
+  try {
+    // getting the data from Alpha Vantage API
+    const response = await axios.get("https://www.alphavantage.co/query", {
+      params: {
+        function: "TIME_SERIES_INTRADAY",
+        symbol: "TSLA",
+        interval: "5min",
+        apikey: financeKey,
+      },
+    });
+    // API call frequency is 5 calls per minute and 500 calls per day.
+    if (!response.data.hasOwnProperty("Note"))
+      // emitting a new message. It will be consumed by the client
+      socket.emit("Finance", response.data);
+  } catch (error) {
+    console.error(`Finance Error: ${error.code}`);
+  }
 };
 
 // make the call and emit every x interval
-const handleUpdate = socket => {
-    if (interval) {
-        clearInterval(interval);
-    }
+const handleUpdate = (socket) => {
+  if (interval) {
+    clearInterval(interval);
+  }
 
-    interval = setInterval(() => this.getFinanceApiAndEmit(socket), updateTime);
-}
+  interval = setInterval(() => this.getFinanceApiAndEmit(socket), updateTime);
+};
 
 module.exports.getFinanceApiAndEmit = getFinanceApiAndEmit;
 module.exports.handleUpdate = handleUpdate;
--- news-portal/src/components/Finance/Finance.js
+++ news-portal/src/components/Finance/Finance.js
@@ -1,53 +1,74 @@
-import React, { PureComponent } from 'react';
-import Card from 'react-bootstrap/Card';
-import LineChart from './LineChart';
+import React, { PureComponent } from "react";
+import Card from "react-bootstrap/Card";
+import LineChart from "./LineChart";
+import teslaMotors from "../../assets/images/finance/tesla-motors.svg";
 
 /**
- * The Finance component widget displays a stock market line chart. 
+ * The Finance component widget displays a stock market line chart.
  * This tile should update every 5 minutes.
  */
 class Finance extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.label = '';    // Symbol
-        this.labels = [];   // x Axis - the time interval
-        this.data = [];     // Points on the chart - close values
-        this.lastUpdatedDateTime = '';
+  constructor(props) {
+    super(props);
+    this.label = ""; // Symbol
+    this.labels = []; // x Axis - the time interval
+    this.data = []; // Points on the chart - close values
+    this.lastUpdatedDateTime = "";
+  }
+
+  render() {
+    //console.log("[Finance.js] render");  // for debug
+
+    if (this.props.response) {
+      this.label = this.props.response["Meta Data"]["2. Symbol"];
+      this.lastUpdatedDateTime = this.props.response["Meta Data"][
+        "3. Last Refreshed"
+      ];
+      let timeSeries = this.props.response["Time Series (5min)"];
+      // filter labels by same date
+      this.labels = Object.keys(timeSeries).filter(
+        (datetime) =>
+          datetime.split(" ")[0] === this.lastUpdatedDateTime.split(" ")[0]
+      );
+      // take the close values sliced by same date
+      this.data = Object.values(timeSeries)
+        .slice(0, this.labels.length)
+        .map((d) => d["4. close"]);
     }
 
-    render() {
-        //console.log("[Finance.js] render");  // for debug
-        
-        if (this.props.response) {
-            this.label = this.props.response['Meta Data']['2. Symbol'];
-            this.lastUpdatedDateTime = this.props.response['Meta Data']['3. Last Refreshed'];
-            let timeSeries = this.props.response['Time Series (5min)'];
-            // filter labels by same date
-            this.labels = Object.keys(timeSeries).filter(datetime => datetime.split(' ')[0] === this.lastUpdatedDateTime.split(' ')[0]);
-            // take the close values sliced by same date
-            this.data = Object.values(timeSeries).slice(0, this.labels.length).map(d => d['4. close']);
-        }
-
-        return (
-            <Card className="col-md-4" border="dark">
-                <Card.Body>
-                    <Card.Title><b>Finance</b></Card.Title>
-                    {!this.props.response ? <Card.Text>Loading...</Card.Text> :
-                        <div>
-                            <LineChart
-                                label={this.label}
-                                labels={this.labels}
-                                data={this.data}
-                            />
-                            <Card.Text>                            
-                                <small className="text-muted">Updated {this.lastUpdatedDateTime}</small>
-                            </Card.Text>
-                        </div>
-                    }
-                </Card.Body>
-            </Card>
-        );
-    }
+    return (
+      <Card className="col-md-4" border="dark">
+        <Card.Body>
+          <Card.Title>
+            <b>Finance</b>
+            {this.props.response ? (
+              <img
+                src={teslaMotors}
+                alt={"Tesla-Motors"}
+                className="small"
+              ></img>
+            ) : null}
+          </Card.Title>
+          {!this.props.response ? (
+            <Card.Text>Loading...</Card.Text>
+          ) : (
+            <div>
+              <LineChart
+                label={this.label}
+                labels={this.labels}
+                data={this.data}
+              />
+              <Card.Text>
+                <small className="text-muted">
+                  Updated {this.lastUpdatedDateTime}
+                </small>
+              </Card.Text>
+            </div>
+          )}
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 export default Finance;
