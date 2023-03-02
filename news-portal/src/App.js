b06fc11faed6018461784a5cd41f0d86b666016c
Dima Kolyas <dimakol@gmail.com>
2023-03-02 11:29:23 +0200

    refactor(App): add data to debug - display in debug logs the data that come from the socket.

--- news-portal/src/App.js
+++ news-portal/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
-import Grid from './components/Grid';
-//import { currentTime } from './utils/timeUtils';  // for debug
-import './App.css';
+import Grid from "./components/Grid";
+//import { currentTime } from "./utils/timeUtils"; // for debug
+import "./App.css";
 
 /**
  * The main application component.
@@ -19,48 +19,51 @@ class App extends Component {
       // the finance data that we are receiving from the server.
       financeResponse: false,
       // the sports data that we are receiving from the server.
-      sportsResponse: false
+      sportsResponse: false,
     };
   }
 
   /**
    * Listening for the events from the server-side,
-   * when some event received storing the data inside our component state. 
+   * when some event received storing the data inside our component state.
    */
   componentDidMount() {
     // making socket request to the server endpoint.
-    const socket = socketIOClient('/');
+    const socket = socketIOClient("/");
     // on news event
-    socket.on("News", data => {
-      //console.log("[App.js] News Update: ", currentTime());  // for debug
-      this.setState({ newsResponse: data })
+    socket.on("News", (data) => {
+      //console.log("[App.js] News Update: ", data, currentTime()); // for debug
+      this.setState({ newsResponse: data });
     });
     // on weather event
-    socket.on("Weather", data => {
-      //console.log("[App.js] Weather Update: ", currentTime());   // for debug
-      this.setState({ weatherResponse: data })
+    socket.on("Weather", (data) => {
+      //console.log("[App.js] Weather Update: ", data, currentTime());   // for debug
+      this.setState({ weatherResponse: data });
     });
     // on finance event
-    socket.on("Finance", data => {
-      //console.log("[App.js] Finance Update: ", currentTime());   // for debug
-      this.setState({ financeResponse: data })
+    socket.on("Finance", (data) => {
+      //console.log("[App.js] Finance Update: ", data, currentTime()); // for debug
+      this.setState({ financeResponse: data });
     });
     // on sports event
-    socket.on("Sports", data => {
-      //console.log("[App.js] Sports Update: ", currentTime());  // for debug
-      this.setState({ sportsResponse: data })
+    socket.on("Sports", (data) => {
+      //console.log("[App.js] Sports Update: ", data, currentTime()); // for debug
+      this.setState({ sportsResponse: data });
     });
   }
 
   render() {
-    const { weatherResponse, newsResponse, financeResponse, sportsResponse } = this.state;
+    const { weatherResponse, newsResponse, financeResponse, sportsResponse } =
+      this.state;
     return (
       <div>
         <h4 className="padding-10 center-text red underline">News Portal</h4>
-        <Grid newsInfo={newsResponse} 
-              weatherInfo={weatherResponse} 
-              financeInfo={financeResponse}
-              sportsInfo={sportsResponse} />
+        <Grid
+          newsInfo={newsResponse}
+          weatherInfo={weatherResponse}
+          financeInfo={financeResponse}
+          sportsInfo={sportsResponse}
+        />
       </div>
     );
   }
