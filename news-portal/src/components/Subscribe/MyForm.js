88eb28dbcd3e4db69f5cfd5375206dc473850750
Dima Kolyas <dimakol@gmail.com>
2023-12-11 12:14:55 +0200

    refactor(MyForm): component incoming props

--- news-portal/src/components/Subscribe/MyForm.js
+++ news-portal/src/components/Subscribe/MyForm.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Button from "react-bootstrap/Button";
 
 /**
  * The form of the subscription.
- * @param {*} props 
+ * @param {function} handleSubmit
+ * @param {function} handleUserInput
+ * @param {string} email
  */
-const MyForm = props => {
-    return (
-        <form className="padding-top-10" onSubmit={props.handleSubmit}>
-            <input className="col-md-12" 
-                    type="email"
-                    placeholder="your@email.com"
-                    required
-                    value={props.email}
-                    onChange={props.handleUserInput} />
-            <Button variant="outline-primary"
-                    size="sm"
-                    className="offset-5 margin-top-20"
-                    type="submit"> SEND
-            </Button>
-        </form> 
-    );
-}
+const MyForm = ({ handleSubmit, handleUserInput, email }) => (
+  <form className="padding-top-10" onSubmit={handleSubmit}>
+    <input
+      className="col-md-12"
+      type="email"
+      placeholder="your@email.com"
+      required
+      value={email}
+      onChange={handleUserInput}
+    />
+    <Button
+      variant="outline-primary"
+      size="sm"
+      className="offset-5 margin-top-20"
+      type="submit"
+    >
+      {" "}
+      SEND
+    </Button>
+  </form>
+);
 
 export default MyForm;
